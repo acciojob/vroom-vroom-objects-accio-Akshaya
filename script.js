89438a670a19e3e512e44c35194ef4a878711b43
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-11-28 02:03:50 -0800

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Car(make, model) {
 	this.make = make;
 	this.model = model;
 }
-Car.prototype.getMakeModel = function{
+Car.prototype.getMakeModel = function(){
 	return `${this.make} ${this.model}`;
 }
 
@@ -11,8 +11,8 @@ function SportsCar(make, model, topSpeed) {
 	Car.call(this,make,model);
 		this.topSpeed = topSpeed;
 }
-SportsCar.prototype = object.create(Car.prototype);
-SportsCar.protoype.constructor = SportsCar;
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor=SportsCar;
 
 SportsCar.prototype.getTopSpeed = function(){
 	return this.topSpeed ;
